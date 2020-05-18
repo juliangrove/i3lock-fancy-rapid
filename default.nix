040270acfaefad5c7f719abd2f0694cb33a9b7a4
Julian Grove <julian.grove@gmail.com>
2020-05-17 20:46:01 -0400

    New default.nix

--- default.nix
+++ default.nix
@@ -1,27 +1,25 @@
 { stdenv, fetchFromGitHub, xorg, i3lock-color }:
 
 stdenv.mkDerivation rec {
-  rev = "e9f7db176325fc92a7c90d040ddf283074957595";
-
   name = "i3lock-fancy-rapid";
 
   src = fetchFromGitHub {
     owner = "juliangrove";
     repo = "i3lock-fancy-rapid";
-    inherit rev;
-    sha256 = "0v37lzzawn26w7dx07ppba74yrl1cbgja9sxrc43mxyfl4l0nm4n";
+    rev = "23a8dce06d7d34ce2134a184bf6585a2dfd48279";
+    sha256 = "1igdx3flj75fvsnw7pcwqnbnm7bhshrcbhpwpsq6vbk8qgndj6xw";
   };
 
   patchPhase = ''
     sed -i "s|i3lock-color|${i3lock-color}/bin/i3lock-color|g" i3lock-fancy-rapid.c
-'';
-    
+  '';
+
   buildInputs = [ xorg.libX11 ];
-  
+
   installPhase = ''
     mkdir -p $out/bin
     cp i3lock-fancy-rapid $out/bin/
-'';
+  '';
 
   meta = {
     description = "A fork of i3lock-fancy-rapid that uses i3lock-color instead of i3lock.";
